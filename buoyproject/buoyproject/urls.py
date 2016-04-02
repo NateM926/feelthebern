4d39859f0fec55e5f2df53b0d425fb6896d89092
Nate <ntmathis@maine.rr.com>
2016-04-01 22:47:29 -0400

    added folders with html and css. Trying to hook everything together with PoS

--- buoyproject/buoyproject/urls.py
+++ buoyproject/buoyproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^pos',views.PoSForm,name='PoSForm'),
+    url(r'^dataentry',views.DataEntryForm,name='DataEntryForm'),
+
 ]
