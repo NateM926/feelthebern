f9f5b0e546114f474d667c81c6f5f64af93a05b5
Nate <ntmathis@maine.rr.com>
2016-04-01 22:25:33 -0400

    working on forms.py

---
+++ buoyproject/tempapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import #MODELNAME
+
+class PoSForm(forms.Form):
+
+class DataEntryForm(forms.Form):
+
+class StatsForm(forms.Form):
+
+class PredictForm(forms.Form):
+
